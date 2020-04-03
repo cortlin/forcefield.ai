46f7707ccf393e95edd82031f1a794f4ba133eac
Cortlin Sawyer <cortlin@forcefield.ai>
2020-04-02 20:26:03 -0500

    Replacing old project with new version

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='context'>
+      <div className='bowl'>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
